3e16b329987f6825a86e726e077a20e5330f111f
Luis <tennis.bresil@gmail.com>
2022-07-30 20:29:23 -0400

    aw
    awww

---
+++ Film_moi/model/Film_Location.class.php
@@ -0,0 +1,40 @@
+<?php
+
+	Class Film_Location 
+	{
+
+		private $PK_ID_Film;
+		private $PK_ID_Location;
+		private $PK_ID_Membre;
+
+		function __Construct($PK_ID_Film, $PK_ID_Location, $PK_ID_Membre)
+		{
+			$this->PK_ID_Film 	   = $PK_ID_Film;
+            $this->PK_ID_Location  = $PK_ID_Location;
+            $this->PK_ID_Membre  = $PK_ID_Membre;
+		}
+
+		
+
+		function getFilmID(){
+			return $this->PK_ID_Film;
+		}
+		function setFilmID($PK_ID_Film){
+			 $this->PK_ID_Film = $PK_ID_Film;
+		}
+		function getLocationID(){
+			return $this->PK_ID_Location;
+		}
+		function setLocationID($PK_ID_Location){
+			 $this->PK_ID_Location = $PK_ID_Location;
+		}
+		function getMembreID(){
+			return $this->PK_ID_Membre;
+		}
+		function setMembreID($PK_ID_Membre){
+			 $this->PK_ID_Membre = $PK_ID_Membre;
+		}
+
+
+	}//fin class
+
